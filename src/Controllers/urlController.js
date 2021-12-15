573d788160d36ee621b8f4898cf5e0ac4688bd54
ashutoshpahariya <ashutoshpahariya6@gmail.com>
2021-12-15 21:44:58 +0530

    urlprojectdata

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -19,6 +19,7 @@ redisClient.auth("GLhI3EzZ2sCFeKyyKRQzdHPGWOoqgoDg", function (err) {
 redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 })
+
 //----------------REDIS VARIABLE
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
 const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
@@ -38,7 +39,6 @@ const baseUrl = "http://localhost:3000";
 
 //-----------FIRST API GENERATE SHORT URL
 const createurl = async function (req, res) {
-
   try {
     if (!isValidRequestBody(req.body)) {
       res.status(400).send({ status: false, message: 'Invalid request parameters. Please provide URL details' })
@@ -47,8 +47,8 @@ const createurl = async function (req, res) {
     if (!isValid(req.body.longUrl)) {
       return res.status(400).send({ status: false, message: ' Please provide LONG URL' })
     }
-
     const longUrl = req.body.longUrl.trim()
+
     ///----URL VALIDATION
     const validUrl = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
     if (!(longUrl.match(validUrl))) {
@@ -56,8 +56,10 @@ const createurl = async function (req, res) {
     }
 
     const baseUrl = 'http://localhost:3000'
+
     //---GENERATE URLCODE
     let urlCode = shortid.generate().match(/[a-z\A-Z]/g).join("") //---this will give only Alphabet
+    urlCode = urlCode.toLowerCase()//---this will gives only lowercase alphabet
 
     //---FETCH THE DATA IN REDIS
     let checkforUrl = await GET_ASYNC(`${longUrl}`)
@@ -65,6 +67,7 @@ const createurl = async function (req, res) {
       console.log("line no66")
       return res.status(200).send({ status: true, "data": JSON.parse(checkforUrl) })
     }
+
     //---FETCH THE DATA IN MONGO DB IF IT IS NOT PRESENT IN CACHE
     let url = await urlModel.findOne({ longUrl })
     if (url) {
@@ -75,71 +78,48 @@ const createurl = async function (req, res) {
     const shortUrl = baseUrl + '/' + urlCode
     const urlData = { urlCode, longUrl, shortUrl }
     const newurl = await urlModel.create(urlData);
+
     //---SET GENERATE DATA IN CACHE
     await SET_ASYNC(`${longUrl}`, JSON.stringify(urlData))
     return res.status(201).send({ status: true, msg: `URL created successfully`, data: newurl });
-  } catch (err) {
+  } 
+  catch (err) {
     console.log(err)
     res.status(500).send({ status: false, msg: 'Server Error' })
   }
 }
 
 
+
+
 //-----------SECOND API PULL LONG URL BY REDIRECTING
-// const geturl = async function (req, res) {
-//   try {
-//       const urlCode = req.params.urlCode.trim().toLowerCase()
-//       console.log(urlCode,"hello 92")
-//       if (!isValid(urlCode)) {
-//           res.status(400).send({ status: false, message: 'Please provide valid urlCode' })
-//       }
-//       console.log(urlCode,"hello 96")
-//       let checkforUrl = await GET_ASYNC(`${urlCode}`)    //first check in cache
-//       console.log(checkforUrl,"hello 98")
-//       if (checkforUrl) {
-//           return res.redirect(302, checkforUrl)
-//       }
-
-//       const url = await urlModel.findOne({ urlCode: urlCode }) //second check in Db
-//       if (!url) {
-//           return res.status(404).send({ status: false, message: 'No URL Found' })
-//       }
-//       await SET_ASYNC(`${urlCode}`, JSON.stringify(url.longUrl)) //if data found in db than created in cache
-//       return res.redirect(302, url.longUrl)
-
-//   } catch (err) {
-//       console.error(err)
-//       res.status(500).send('Server Error')
-//   }
-// }
-
- const geturl = async function (req, res) {
+const geturl = async function (req, res) {
   try {
     const urlCode = req.params.urlCode.trim().toLowerCase()
     if (!isValid(urlCode)) {
       res.status(400).send({ status: false, message: 'Please provide valid urlCode' })
       console.log()
     }
+
     //---FETCH THE DATA BY URLCODE IN REDIS
     let checkforUrl = await GET_ASYNC(`${urlCode}`)
-    console.log(checkforUrl, "hiii")
     if (checkforUrl) {
       return res.redirect(302, checkforUrl)
-      
     }
+
     //---FETCH THE DATA IN MONGO DB IF IT IS NOT PRESENT IN CACHE
     const url = await urlModel.findOne({ urlCode: urlCode })
     if (!url) {
       return res.status(404).send({ status: false, message: 'No URL Found' })
     }
-      //---SET GENERATE DATA IN CACHE
-      await SET_ASYNC(`${urlCode}`, JSON.stringify(url.longUrl))
+
+    //---SET GENERATE DATA IN CACHE
+    await SET_ASYNC(`${urlCode}`, JSON.stringify(url.longUrl))
     return res.redirect(302, url.longUrl)
   } catch (err) {
-    console.error(err)
     res.status(500).send('Server Error')
-  } }
+  }
+}
 
 module.exports.createurl = createurl
 module.exports.geturl = geturl
-
--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose");
-
+//----PROJECT THREE MONGO DB STRING NAMED ---group3DataBase
 mongoose
-    .connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/ashutosh_pahariya_db?authSource=admin&replicaSet=atlas-e7145j-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+    .connect("mongodb+srv://user-open-to-all:hiPassword123@cluster0.xgk0k.mongodb.net/group3DataBase?authSource=admin&replicaSet=atlas-e7145j-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
         { useNewUrlParser: true }
     )
     .then(() => {
@@ -23,3 +23,12 @@ mongoose
     .catch((err) => console.log(err));
 
 app.use("/", route);
+
+
+
+
+
+
+
+
+
